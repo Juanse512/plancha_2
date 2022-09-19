d7ddbfc34a18cba732b490810db69e3bad6922a1
Juanse512 <juanseval1@gmail.com>
2022-09-19 15:25:11 -0300

    1 y 3

---
+++ Ej 1/EJ1_1.c
@@ -0,0 +1,4 @@
+//El codigo de salida es de 0377(0xFF)
+int main (){    
+    return 0xFF;
+}
---
+++ Ej 1/EJ1_2.c
@@ -0,0 +1,7 @@
+//El codigo de salida es de 0375(0xFD)
+int main (){
+    int a=0xFE,b=-1;
+    a=a+b;
+    b++;
+    return a;
+}
